9bdf464a4780c046f378416a54a4d002e021dac4
Sifa Gakeni Muriithi <gakenisifa@gmail.com>
2024-10-01 23:31:07 +0300

    Array lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+//      APPEND
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+destructivelyAppendCat("Ralph")
+
+
+//    PREPEND
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+destructivelyPrependCat("Bob")
+
+
+//     REMOVE LAST ELEMENT
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+destructivelyRemoveLastCat()
+
+
+//     REMOVE FIRST ELEMENT
+function destructivelyRemoveFirstCat(){
+   cats.shift();
+}
+destructivelyRemoveFirstCat()
+
+//    SPREAD OPERATOR
+function appendCat(name){
+    return [...cats, name];
+}
+appendCat("Broom")
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+prependCat("Arnold")
+
+//    .SLICE()
+function removeLastCat(){
+    return[...cats.slice(0,2)];
+    // return [...cats.slice(0, -1)];
+}
+removeLastCat();
+console.log(removeLastCat())
+
+function removeFirstCat(){
+    return[...cats.slice(1)];
+}
+removeFirstCat();
+console.log(removeFirstCat())
